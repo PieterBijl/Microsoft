cc564f5a2df9b18469137c7ee349ec293835ac52
PieterBijl <pieterbijl1@gmail.com>
2020-05-20 10:43:45 +0200

    changed last stuff for visualisation

--- Microsat_VBAR_Scenario/early_visualisation.m
+++ Microsat_VBAR_Scenario/early_visualisation.m
@@ -5,13 +5,13 @@ data = table2array(readtable('features_data.txt'));
 
 x1 = data(:,1);
 y1 = data(:,2);
-f1 = figure;
-title('Visualisation of the 16 features in x and y over time')
-xlabel('x') 
-ylabel('y') 
+f1 = figure; 
 for i = 1:16
     plot(data(:,i),data(:,i+1))
     hold on
     plot(data(1,i),data(1,i+1),'r*')
 end
+title('Visualisation of the 16 features in x and y over time')
+xlabel('x') 
+ylabel('y')
 %legend('1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16')
