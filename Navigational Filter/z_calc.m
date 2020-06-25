35b1aeb58d8ed2ac140bd5b9c87127a85ee0442f
PieterBijl <pieterbijl1@gmail.com>
2020-06-25 16:02:30 +0200

    small change to z

--- Navigational Filter/z_calc.m
+++ Navigational Filter/z_calc.m
@@ -2,6 +2,7 @@ function z = z_calc(datapoints,n,std_noise)
 % This functions returns the measurements with perspective correction 
     pixel_size = 1.1*10^-5;
     for i=1:2*n
-        z(i,1) = pixel_size*(0.5*datapoints(i)+128+std_noise);
+        datapoints(i) = datapoints(i) + randn(1,1)*std_noise;
+        z(i,1) = pixel_size*(0.5*datapoints(i)+128);
     end
 end
