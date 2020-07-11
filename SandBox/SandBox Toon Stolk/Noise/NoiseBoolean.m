8484ec55d90b6d327822da2c0e4e92aa96834084
PPins <65160606+PPins@users.noreply.github.com>
2020-07-11 13:07:23 +0200

    filter

--- SandBox/SandBox Toon Stolk/Noise/NoiseBoolean.m
+++ SandBox/SandBox Toon Stolk/Noise/NoiseBoolean.m
@@ -1,9 +1,9 @@
 function bool = NoiseBoolean(betaAngle, eclipseCone, FOVcone)
 %NOISEBOOLEAN Summary of this function goes here
 %   Detailed explanation goes here
-if (pi-eclipseCone/2 <= betaAngle) && (betaAngle <= pi+eclipseCone/2)
+if (pi-eclipseCone/2 < betaAngle) && (betaAngle < pi+eclipseCone/2)
     bool = 0;
-elseif (1.5*pi-FOVcone/2 <= betaAngle) && (betaAngle <= 1.5*pi+FOVcone/2)
+elseif (1.5*pi-FOVcone/2 < betaAngle) && (betaAngle < 1.5*pi+FOVcone/2)
     bool = 0;
 else
     bool = 1;
