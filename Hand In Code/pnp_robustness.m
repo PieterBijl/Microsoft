31a0a7da85184e4a4dc6d9faf0e2350f9b882f1e
PieterBijl <pieterbijl1@gmail.com>
2020-07-12 21:43:20 +0200

    added bunch of stuff

--- Hand In Code/pnp_robustness.m
+++ Hand In Code/pnp_robustness.m
@@ -4,7 +4,6 @@ addpath EPnP
 
 %% Get the data
 load('x_real.mat')
-load('euler_real.mat')
 feature_data = importdata('features_data_extended.txt'); %in pixels
 feature_points = 1/100*importdata('feature_points.txt'); %in meters
 
@@ -35,17 +34,12 @@ for std_noise = 1:6
         e_t_2(std_noise,i) = abs(Tp(2)-x_real(3,i)); % The y value of the EPnP corresponds to the z value of the filter
         e_t_3(std_noise,i) = abs(Tp(3)-x_real(2,i)); % The z value of the EPnP corresponds to the y value of the filter
         beta = quatmultiply(x_real(7:10,i)',q);
-        e_q(std_noise,i) = 2*acos(beta(1));
-        d_eul = abs(euler_real(:,i)-quat2eul(q));
-        e_d_1(std_noise,i) = rad2deg(d_eul(1));
-        e_d_2(std_noise,i) = rad2deg(d_eul(2));
-        e_d_3(std_noise,i) = rad2deg(d_eul(3));
+        e_q(std_noise,i) = 2*acos(abs(beta(1)));
     end
     e_t_m_1(std_noise) = mean(e_t_1(std_noise,:));
     e_t_m_2(std_noise) = mean(e_t_2(std_noise,:));
     e_t_m_3(std_noise) = mean(e_t_3(std_noise,:));
-    e_q_m(std_noise) = abs(rad2deg(mean(e_q(std_noise,:))-pi));
-    e_d_m_1 = mean(e_d_1(st
+    e_q_m(std_noise) = abs(mean(e_q(std_noise,:)));
 end
 
 %% Plotting
@@ -68,7 +62,6 @@ xlabel("Standard noise in pixels")
 ylabel("Error in meters")
 subplot(2,2,4)
 plot(e_q_m)
-title("EPnP Quaternion Error")
+title("EPnP Euler Axis-Angle Error")
 xlabel("Standard noise in pixels")
-ylabel("Attitude Error")
-
+ylabel("Attitude Error in Degrees")
--- SandBox/SandBox Pieter Bijl/Over Again/pnp_robustness_feature_points.m
+++ SandBox/SandBox Pieter Bijl/Over Again/pnp_robustness_feature_points.m
@@ -3,7 +3,7 @@ clear all
 addpath EPnP
 
 %% Get the data
-load('x_real.mat')
+load('x_real.mat') % x real is the state vector taken from the measurements without noise for the entire orbit
 feature_data = importdata('features_data_extended.txt'); %in pixels
 feature_points = 1/100*importdata('feature_points.txt'); %in meters
 
@@ -17,14 +17,14 @@ fy = fx;
 m = 1.1*10^-5;
 A=[fx/m 0 u0; 0 fy/m v0; 0 0 1];
 std_noise = 2;
-for n_reduction = 1:12
-    for i = 1:6000
+for n_reduction = 1:12 % n_reduction stands for the number of points that will be removed
+    for i = 1:6000 % for 100 minutes
         x3d_h=zeros(n-n_reduction,4);
         x2d_h=zeros(n-n_reduction,3);
-        y = sort(datasample(1:16,16-n_reduction,'Replace',false));
-        wireframe_reduced = zeros(3,16-n_reduction);
-        feature_data_reduced = zeros(32-2*n_reduction,1);
-        for k = 1:length(y)
+        y = sort(datasample(1:16,16-n_reduction,'Replace',false)); % the datapoints that will still be used for EPnP
+        wireframe_reduced = zeros(3,16-n_reduction); % create the 'reduced' wireframe matrix
+        feature_data_reduced = zeros(32-2*n_reduction,1); % create the 'reduced' measurement vecotr
+        for k = 1:length(y) % This loop fills the 'reduced' wireframe matrix and the 'reduced' measurement vector
             wireframe_reduced(:,k) = feature_points(:,y(k));
             feature_data_reduced(2*k-1) = feature_data(i,2*y(k)-1);
             feature_data_reduced(2*k) = feature_data(i,2*y(k));
@@ -44,6 +44,7 @@ for n_reduction = 1:12
         beta = quatmultiply(x_real(7:10,i)',q);
         e_q(n_reduction,i) = 2*acos(abs(beta(1)));
     end
+    % Calculate the mean for every n_reduction
     e_t_m_1(n_reduction) = mean(e_t_1(n_reduction,:));
     e_t_m_2(n_reduction) = mean(e_t_2(n_reduction,:));
     e_t_m_3(n_reduction) = mean(e_t_3(n_reduction,:));
